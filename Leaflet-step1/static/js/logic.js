0c59ba4fc1efd9844b2214061aea6cda3aacfbad
qinqin zha <qinqin.zha@gmail.com>
2020-10-23 18:56:03 -0700

    step1 commit

--- Leaflet-step1/static/js/logic.js
+++ Leaflet-step1/static/js/logic.js
@@ -1,14 +1,16 @@
 var myMap = L.map("mapid", {
-  center: [37.773972, -122.431297],
-  zoom: 8
+  center: [34.0522, -118.2437],
+  zoom: 7
 });
 
+// Add a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox.streets",
+  id: "mapbox/streets-v11",
   accessToken: API_KEY
 }).addTo(myMap);
 
@@ -58,8 +60,7 @@ d3.json(url, function(response) {
   legend.onAdd = function(){
     var div = L.DomUtil.create('div', 'info legend'),
       grades = [0, 1, 2, 3, 4, 5],
-    // var limits = geojson.options.limits;
-    // var colors = geojson.options.colors;
+    
       labels = [];
     for (var i = 0; i < grades.length; i++) {
       div.innerHTML +=
